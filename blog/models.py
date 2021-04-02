db64407ff18ed5989adf2ad05cb48fa3a59b3a10
amin <amin.ghanee@gmail.com>
2021-04-02 18:25:45 +0430

    insert __str__ self

--- blog/models.py
+++ blog/models.py
@@ -7,8 +7,8 @@ from ckeditor.fields import RichTextField
 def validate_files_extension(value):
     import os
     from django.core.exceptions import ValidationError
-    ext = os.path.abspath(value.name)[1]
-    valid_extension = ['.png', '.jpeg']
+    ext = os.path.splitext(value.name)[1]
+    valid_extension = ['.png', '.jpg']
     if not ext.lower() in valid_extension:
         raise ValidationError('unsupported file extension.')
 
@@ -17,6 +17,9 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     avatar = models.FileField(upload_to='files/user_avatar/', null=True, blank=True, validators=[validate_files_extension])
     description = models.CharField(max_length= 512, null=False, blank=False)
+
+    def __str__(self):
+        return self.user.username
 
 
 class Article(models.Model):
@@ -27,7 +30,13 @@ class Article(models.Model):
     category = models.ForeignKey('Category', on_delete=models.CASCADE)
     author = models.OneToOneField(UserProfile, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.title
+
 
 class Category(models.Model):
     title = models.CharField(max_length=128, null=False, blank=False)
     cover = models.FileField(upload_to='files/category_cover/', null=False, blank=False, validators=[validate_files_extension])
+
+    def __str__(self):
+        return self.title
